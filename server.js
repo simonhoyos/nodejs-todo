af8f98a34652ec0ac5e18e7a5e389977ff519ed4
shmesa22 <shmesa23@gmail.com>
2017-07-14 10:41:40 -0500

    Fix server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 var mongoose = require('mongoose');
 var Task = require('./api/models/todoListModel');
 var bodyParser = require('body-parser');
