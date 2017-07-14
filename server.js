4bca6c523fb66a7a2b88602c90a5ab89800f4c39
shmesa22 <shmesa23@gmail.com>
2017-07-14 10:31:00 -0500

    Register routes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3001;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var Task = require('./api/models/todoListModel');
+var bodyParser = require('body-parser');
+  
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb', {useMongoClient: true}); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.listen(port);
 
